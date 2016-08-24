ae41a3f14d7f82017467ad6b2b3df2f4c6118572
unknown <jacek.pardyak@gmail.com>
2016-08-24 13:38:53 +0200

    First release

---
+++ R/cover_letter.R
@@ -0,0 +1,72 @@
+#' Journal of Statistical Software (JSS) format.
+#'
+#' Format for creating a Journal of Statistical Software (JSS) articles. Adapted
+#' from
+#' \href{http://www.jstatsoft.org/about/submissions}{http://www.jstatsoft.org/about/submissions}.
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Arguments to \code{rmarkdown::pdf_document}
+#'
+#' @return R Markdown output format to pass to
+#'   \code{\link[rmarkdown:render]{render}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("MyArticle.Rmd", template = "cover_letter", package = "resumeR")
+#' }
+#'
+#' @export
+cover_letter <- function(..., keep_tex = TRUE) {
+
+  template <- find_resource("cover_letter", "template.tex")
+
+  base <- inherit_pdf_document(..., template = template, keep_tex = keep_tex)
+
+  # Mostly copied from knitr::render_sweave
+  base$knitr$opts_knit$out.format <- "sweave"
+
+  base$knitr$opts_chunk$prompt <- TRUE
+  base$knitr$opts_chunk$comment <- NA
+  base$knitr$opts_chunk$highlight <- FALSE
+
+  base$knitr$opts_chunk$dev.args <- list(pointsize = 11)
+  base$knitr$opts_chunk$fig.width <- 4.9 # 6.125" * 0.8, as in template
+  base$knitr$opts_chunk$fig.height <- 3.675 # 4.9 * 3:4
+  base$knitr$opts_chunk$fig.align <- "center"
+  hook_chunk <- function(x, options) {
+    if (output_asis(x, options)) return(x)
+    paste0('\\begin{CodeChunk}\n', x, '\\end{CodeChunk}')
+  }
+  save <- options(prompt = "R> ", continue = "R+ ")
+  hook_input <- function(x, options) {
+    if (options$prompt && length(x)) {
+      x <- gsub("\\n", paste0("\n", getOption("continue")), x)
+      x <- paste0(getOption("prompt"), x)
+    }
+    paste0(c('\n\\begin{CodeInput}', x, '\\end{CodeInput}', ''),
+      collapse = '\n')
+  }
+  hook_output <- function(x, options) {
+    paste0('\n\\begin{CodeOutput}\n', x, '\\end{CodeOutput}\n')
+  }
+  old_hook <- base$knitr$knit_hooks$document
+  hook_document <- function(x) {
+    options(save)
+    if (is.function(old_hook))
+      old_hook(x)
+    else
+      base::identity(x)
+  }
+  base$knitr$knit_hooks$chunk   <- hook_chunk
+  base$knitr$knit_hooks$source  <- hook_input
+  base$knitr$knit_hooks$output  <- hook_output
+  base$knitr$knit_hooks$message <- hook_output
+  base$knitr$knit_hooks$warning <- hook_output
+  base$knitr$knit_hooks$plot <- knitr::hook_plot_tex
+  base$knitr$knit_hooks$document <- hook_document
+
+  base
+}
+
---
+++ tests/testthat/test_formats.R
@@ -0,0 +1,44 @@
+
+context("Formats")
+
+test_format <- function(name, file_check = TRUE, os_skip = NULL) {
+
+  test_that(paste(name, "format"), {
+
+    # don't run on cran because pandoc is required
+    skip_on_cran()
+
+    # skip on os if requested
+    if (!is.null(os_skip))
+      skip_on_os(os_skip)
+
+    # work in a temp directory
+    dir <- tempfile()
+    dir.create(dir)
+    oldwd <- setwd(dir)
+    on.exit(setwd(oldwd), add = TRUE)
+
+    # create a draft of the format
+    testdoc <- "testdoc.Rmd"
+    rmarkdown::draft(testdoc,
+                     system.file("rmarkdown", "templates", name,
+                                 package = "resume"),
+                     create_dir = FALSE,
+                     edit = FALSE)
+
+    # render it
+    capture.output({
+      if (file_check) {
+        output_file <- tempfile(fileext = ".pdf")
+        rmarkdown::render(testdoc, output_file = output_file)
+        expect_true(file.exists(output_file))
+      } else {
+        rmarkdown::render(testdoc)
+      }
+    })
+  })
+}
+
+
+test_format("cover_letter")
+test_format("resume")
