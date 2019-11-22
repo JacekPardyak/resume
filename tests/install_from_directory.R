c778cbcf018fd4206051069c110befdec24263ca
Pardyak <jacek.pardyak@atos.net>
2019-11-22 19:10:47 +0100

    ok

--- tests/install_from_directory.R
+++ tests/install_from_directory.R
@@ -1,3 +1,6 @@
+# install from directory
 install.packages("./", 
                  repos = NULL, 
                  type = "source")
+# install from gitHub
+devtools::install_github("jacekpardyak/resume")
