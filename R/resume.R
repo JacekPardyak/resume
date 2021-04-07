0966915f4060f13cda844fa930d466dc09ba6b29
unknown <jacek.pardyak@gmail.com>
2021-04-07 18:34:53 +0200

    New R scripts

--- R/resume.R
+++ R/resume.R
@@ -1,71 +1,36 @@
-#' Template to build Resume.
+#' Steve's Statement Template
 #'
-#' First create a new R Markdown document from template Resume then modify it 
-#' and knit PDF output.
+#' A template for what I term "statements." These are things like research statements and teaching statements when I'm
+#' applying for jobs or doing end-of-the-semester busy work for the proverbial "ball and chain."
+#'
+#' # About YAML header fields
+#'
+#' This section documents some of the YAML fields to know
+#' for this template.
+#'
+#'
+#' | FIELD  | DESCRIPTION |
+#' | ------ | ----------- |
+#' | `title` | title of the document, appears on first row |
+#' | `author` | name of the author, appears on second row |
+#' | `affiliation` | institutional affiliation, appears on third row |
+#' | `email` | your email, for the fourth row |
 #'
 #' @inheritParams rmarkdown::pdf_document
-#' @param ... Arguments to \code{rmarkdown::pdf_document}
-#'
-#' @return R Markdown output format to pass to
-#'   \code{\link[rmarkdown:render]{render}}
-#'
-#' @examples
-#'
-#' \dontrun{
-#' library(rmarkdown)
-#' draft("resume.Rmd", template = "resume", package = "resume")
-#' }
-#'
+#' @param ... Arguments to [`rmarkdown::pdf_document`].
+#' @md
 #' @export
-resume <- function(..., keep_tex = TRUE) {
+#'
 
-  template <- find_resource("resume", "template.tex")
-
-  base <- inherit_pdf_document(..., template = template, keep_tex = keep_tex)
-
-  # Mostly copied from knitr::render_sweave
-  base$knitr$opts_knit$out.format <- "sweave"
-
-  base$knitr$opts_chunk$prompt <- TRUE
-  base$knitr$opts_chunk$comment <- NA
-  base$knitr$opts_chunk$highlight <- FALSE
-
-  base$knitr$opts_chunk$dev.args <- list(pointsize = 11)
-  base$knitr$opts_chunk$fig.width <- 4.9 # 6.125" * 0.8, as in template
-  base$knitr$opts_chunk$fig.height <- 3.675 # 4.9 * 3:4
-  base$knitr$opts_chunk$fig.align <- "center"
-  hook_chunk <- function(x, options) {
-    if (output_asis(x, options)) return(x)
-    paste0('\\begin{CodeChunk}\n', x, '\\end{CodeChunk}')
-  }
-  save <- options(prompt = "R> ", continue = "R+ ")
-  hook_input <- function(x, options) {
-    if (options$prompt && length(x)) {
-      x <- gsub("\\n", paste0("\n", getOption("continue")), x)
-      x <- paste0(getOption("prompt"), x)
-    }
-    paste0(c('\n\\begin{CodeInput}', x, '\\end{CodeInput}', ''),
-      collapse = '\n')
-  }
-  hook_output <- function(x, options) {
-    paste0('\n\\begin{CodeOutput}\n', x, '\\end{CodeOutput}\n')
-  }
-  old_hook <- base$knitr$knit_hooks$document
-  hook_document <- function(x) {
-    options(save)
-    if (is.function(old_hook))
-      old_hook(x)
-    else
-      base::identity(x)
-  }
-  base$knitr$knit_hooks$chunk   <- hook_chunk
-  base$knitr$knit_hooks$source  <- hook_input
-  base$knitr$knit_hooks$output  <- hook_output
-  base$knitr$knit_hooks$message <- hook_output
-  base$knitr$knit_hooks$warning <- hook_output
-  base$knitr$knit_hooks$plot <- knitr::hook_plot_tex
-  base$knitr$knit_hooks$document <- hook_document
-
-  base
+resume <- function(...){
+  templ <- system.file("rmarkdown", "templates", "resume", "resources", "template.tex", package = "resume")
+  rmarkdown::pdf_document(template = templ,
+                          ...)
 }
 
+
+#' @rdname resume
+#' @export
+templ_resume <- function() {
+  print(system.file("rmarkdown", "templates", "resume", "resources", "template.tex", package = "resume"))
+}
