35ba805b73b2fcce2441f93680ae2a160d7440ee
unknown <jacek.pardyak@gmail.com>
2021-04-08 18:00:47 +0200

    Clean

---
+++ R/graphics.R
@@ -0,0 +1,11 @@
+# Way to convert svg files to pdf and png
+# requires Inkscape installed
+
+# files <- list.files("./inst/rmarkdown/templates/curriculum_vitae/skeleton/images/", pattern = ".svg", full.names = T)
+# 
+# for (file in files) {
+#   command <- paste('inkscape  --export-type=png', file, sep = " ")
+#   system(command)
+#   command <- paste('inkscape  --export-type=pdf', file, sep = " ")
+#   system(command)
+# }
